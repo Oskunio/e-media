d0d9dfc8f281e20962d93357aca5f0e508ea9f38
Oskar <podgrzyb123@gmail.com>
2020-03-05 23:12:32 +0100

    naprawione łokno

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 img = cv2.imread('dog.png')
+cv2.namedWindow('dog.png', cv2.WINDOW_NORMAL) #umozliwia rozciaganie okna
 cv2.imshow('dog.png',img)
-cv2.waitKey(0)
+cv2.waitKey(0) #zeby nie zamykalo okna
+cv2.destroyAllWindows()
 
 
 
