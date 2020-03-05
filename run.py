8019856bb7029c1fee54b951e33728806ab2e938
Oskar <podgrzyb123@gmail.com>
2020-03-05 23:45:02 +0100

    +transformata

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import cv2
 import numpy as np
+
 img = cv2.imread('dog.png')
-cv2.namedWindow('dog.png', cv2.WINDOW_NORMAL) #umozliwia rozciaganie okna
-cv2.imshow('dog.png',img)
+
+#robienie transformaty
+f = np.fft.fft2(img)
+magnitude_spectrum = 20*np.log(np.abs(f))
+magnitude_spectrum = np.asarray(magnitude_spectrum, dtype=np.uint8)
+
+cv2.namedWindow('magnitude_spectrum', cv2.WINDOW_NORMAL) #umozliwia rozciaganie okna
+cv2.imshow('magnitude_spectrum',magnitude_spectrum) #wyswietla transformate
+cv2.namedWindow('dog.png', cv2.WINDOW_NORMAL)
+cv2.imshow('dog.png',img) #wyswietla obraz
 cv2.waitKey(0) #zeby nie zamykalo okna
 cv2.destroyAllWindows()
 
