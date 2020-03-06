901a02dc1660f6e58cff0646896d62f9ed0e75e7
jimmeryn <jimmeryn@gmail.com>
2020-03-06 18:14:12 +0100

    Metadata extract
    Added new file to read from. Extract data from it using pyPng

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 import cv2
 import numpy as np
-img = cv2.imread('dog.png')
+
+filename = "square.png"
+img = cv2.imread(filename)
 
 window = "Window"
 # Create a named window
 cv2.namedWindow(window)
 
-# Move it to (40,30)
-cv2.moveWindow(window, 40, 30)
+# Move window to sertain place
+cv2.moveWindow(window, 600, 200)
 
 # Percent of original size
-scale_percent = 30
+scale_percent = 1000
 width = int(img.shape[1] * scale_percent / 100)
 height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
@@ -20,6 +22,16 @@ resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 # Show image
 cv2.imshow(window, resized)
+print(img)
 
-cv2.waitKey(0)
+# for line in open(filename, "rb"):
+#     if 'END' in line:
+# break
+#     if 'LINES' in line:
+#         # remove trailing newline
+#         line = line.rstrip('\n')
+#         # extract integer value
+#         print int(line.split('=')[1])  # 2240
+
+cv2.waitKey()
 cv2.destroyAllWindows()
