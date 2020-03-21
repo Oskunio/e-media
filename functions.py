ba7e51a782d070cfabb15a8077e7a03bdb285dfd
jimmeryn <jimmeryn@gmail.com>
2020-03-21 21:19:27 +0100

    linter

--- functions.py
+++ functions.py
@@ -1,42 +1,52 @@
 import cv2
 # szukanie chunksow i usuwanie ich
 # funkcja przyjmuje nazwe pliku, a zwraca nowy plik tekstowy (obraz)
+
+
 def findAndRemoveAncillaryChunk(filename):
-    #ancillary chunks in hex code
-    chunks = ["624b4744","6348524d","64534947","65584966","67414d41","68495354","69434350","69545874","70485973","73424954","73504c54","73524742","73544552","74455874","74494d45","74524e53","7a545874"]
+    # ancillary chunks in hex code
+    chunks = ["624b4744", "6348524d", "64534947", "65584966", "67414d41",
+              "68495354", "69434350", "69545874", "70485973", "73424954",
+              "73504c54", "73524742", "73544552", "74455874", "74494d45",
+              "74524e53", "7a545874"]
 
     constChunkLengthInBytes = 12
     constChunkLength = 2*constChunkLengthInBytes
 
-    #open and convert file to hex string
+    # open and convert file to hex string
     handler = open(filename, 'rb')
     newTextFile = handler.read().hex()
 
     for element in chunks:
-        posInText = newTextFile.find(element) #if not found returns -1
+        posInText = newTextFile.find(element)  # if not found returns -1
         if posInText != -1:
             length = newTextFile[(posInText-8):posInText]
             chunkLengthDec = int(length, 16)
             realLength = 2*chunkLengthDec
             firstpart = newTextFile[0:(posInText-8)]
-            secondpart = newTextFile[((posInText-8)+realLength+constChunkLength):]
+            secondpart = newTextFile[(
+                (posInText-8)+realLength+constChunkLength):]
             newTextFile = firstpart+secondpart
     print("Ancillary chunks removed")
     handler.close()
     return newTextFile
 
-#wyswietla plik w czytelnym dla czlowieka formacie
+# wyswietla plik w czytelnym dla czlowieka formacie
+
+
 def pritnFileInAnsi(filename):
     file = open(filename, encoding='ANSI')
     f2 = file.read()
     print(f2)
     file.close()
 
+
 def printFileInHex(filename):
     handler = open(filename, 'rb')
     hexFile = handler.read().hex()
     print(hexFile)
     handler.close()
+
 
 def displayImage(filename):
     img = cv2.imread(filename)
@@ -59,14 +69,16 @@ def displayImage(filename):
     cv2.waitKey()
     cv2.destroyAllWindows()
 
+
 def showImgInfo(filename):
     img = cv2.imread(filename)
     shape = img.shape
     # dimensions of picture
-    print("shape:(x,y,color format table size):",shape)
+    print("shape:(x,y,color format table size):", shape)
     # size
     size = img.size
     print("size:", size)
+
 
 def HexStrigToPNG(filename, newFile):
     data = bytes.fromhex(newFile)
@@ -74,8 +86,15 @@ def HexStrigToPNG(filename, newFile):
         file.write(data)
     file.close()
 
-# IHDR data: it contains (in this order) the image's width (4 bytes), height (4 bytes), bit depth (1 byte), color type (1 byte),
-# compression method (1 byte), filter method (1 byte), and interlace method (1 byte) (13 data bytes total)
+# IHDR data: it contains (in this order) the image's width (4 bytes),
+# height (4 bytes),
+# bit depth (1 byte),
+# color type (1 byte),
+# compression method (1 byte),
+# filter method (1 byte),
+# interlace method (1 byte)
+# (13 data bytes total)
+
 
 def IHDRinterpetation(filename):
 
@@ -86,9 +105,9 @@ def IHDRinterpetation(filename):
 
     if posInText != -1:
 
-        length = hexFile[(posInText - 8):posInText]
-        chunkLengthDec = int(length, 16)
-        realLength = 2 * chunkLengthDec
+        # length = hexFile[(posInText - 8):posInText]
+        # chunkLengthDec = int(length, 16)
+        # realLength = 2 * chunkLengthDec
 
         width = hexFile[(posInText+8):(posInText+16)]
         widthDec = int(width, 16)
@@ -98,12 +117,14 @@ def IHDRinterpetation(filename):
         heightDec = int(height, 16)
         print("height:", heightDec)
 
-        # Bit depth is a single-byte integer giving the number of bits per sample or per palette index (not per pixel)
-        bitDepth=hexFile[(posInText + 24):(posInText + 26)]
+        # Bit depth is a single-byte integer giving the number of bits
+        # per sample or per palette index (not per pixel)
+        bitDepth = hexFile[(posInText + 24):(posInText + 26)]
         bitDepthDec = int(bitDepth, 16)
         print("bit depth:", bitDepthDec)
 
-        # color type-img type: 0-grayscale, 2-truecolor, 3-indexed-colour, 4-grayscale with alpha, 6-truecolor with aplha
+        # color type-img type: 0-grayscale, 2-truecolor, 3-indexed-colour,
+        # 4-grayscale with alpha, 6-truecolor with aplha
         color = hexFile[(posInText + 26):(posInText + 28)]
         colorDec = int(color, 16)
         print("color type:", colorDec)
@@ -113,7 +134,7 @@ def IHDRinterpetation(filename):
         compressionDec = int(compression, 16)
         print("compression method:", compressionDec)
 
-        filter = hexFile[(posInText + 30):(posInText + 32)]
+        # filter = hexFile[(posInText + 30):(posInText + 32)]
         filterDec = int(bitDepth, 16)
         print("filter method:", filterDec)
 
@@ -124,6 +145,3 @@ def IHDRinterpetation(filename):
 
     else:
         print("IHDR not found")
-
-
-
--- run.py
+++ run.py
@@ -1,22 +1,18 @@
 import functions
 
-filename = "square.png"
-filename2 = 'newSquare.png'
-#functions.displayImage(filename)
-#functions.showImgInfo(filename)
-#functions.pritnFileInAnsi(filename)
-#functions.printFileInHex(filename)
-#print()
+filename = "./images/square.png"
+filename2 = './images/newSquare.png'
+# functions.displayImage(filename)
+# functions.showImgInfo(filename)
+# functions.pritnFileInAnsi(filename)
+# functions.printFileInHex(filename)
+# print()
 
-#newFile = functions.findAndRemoveAncillaryChunk(filename)
+# newFile = functions.findAndRemoveAncillaryChunk(filename)
 
-#functions.HexStrigToPNG(filename2, newFile)
-#functions.displayImage(filename2)
-#functions.pritnFileInAnsi(filename2)
-#functions.printFileInHex(filename2)
+# functions.HexStrigToPNG(filename2, newFile)
+# functions.displayImage(filename2)
+# functions.pritnFileInAnsi(filename2)
+# functions.printFileInHex(filename2)
 
 functions.IHDRinterpetation(filename)
-
-
-
-
