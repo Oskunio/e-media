b7260779b1e8eb99ea6df69046c83415b0d862a5
jimmeryn <jimmeryn@gmail.com>
2020-03-26 13:00:54 +0100

    Added console prints

--- getExif.py
+++ getExif.py
@@ -7,15 +7,18 @@ from PIL.ExifTags import TAGS
 
 def getExif(filename):
     print("Getting EXIF data from your file:", filename)
+    print("\n Using PyPNG \n")
     image = png.Reader(filename)
     for c in image.chunks():
         print(c[0], len(c[1]))
 
+    print("\n Using hachoir \n")
     parser = createParser(filename)
     metadata = extractMetadata(parser)
     for line in metadata.exportPlaintext():
         print(line)
 
+    print("\n Using Pillow \n")
     image = Image.open(filename)
     for tag, value in image.info.items():
         key = TAGS.get(tag, tag)
